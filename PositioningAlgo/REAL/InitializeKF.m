c30d4bedb21e0e8648da2f1e86281d2693e258f5
christine <christineslcao@gmail.com>
2018-03-05 16:20:06 -0700

    send all coords to GUI

--- PositioningAlgo/REAL/InitializeKF.m
+++ PositioningAlgo/REAL/InitializeKF.m
@@ -4,14 +4,14 @@ A = zeros(size(target));
 w = zeros(size(target,1),1);
 delta = inf;
 
-while (abs(delta) > 0.0001)
+% while (abs(delta) > 0.0001)
     A = DesignMatrix(target, x_0);
     w = Misclosure(x_0,target,obs');
     N = A'*A;
     delta = -1*inv(N)*A'*w;
 
     x_0 = x_0' + delta;
-end
+% end
 
 x_hat = x_0';
 Cx = inv(N);
--- PositioningAlgo/REAL/SentToGui.m
+++ PositioningAlgo/REAL/SentToGui.m
@@ -1,11 +1,23 @@
-function [  ] = SentToGui( roverID, guiclient, KF_Coords, lat_o, long_o, KF_Cx )
-    [lat, long] = GetLatLong(lat_o, long_o, KF_Coords(1,1), KF_Coords(1,2))
-    [er1, er2] = GetError(KF_Cx)
+function [  ] = SentToGui( roverID, guiclient, KF_Coords, lat_o, long_o, KF_Cx, targets, targetList )
+    [lat, long] = GetLatLong(lat_o, long_o, KF_Coords(1,1), KF_Coords(1,2));
+    [er1, er2] = GetError(KF_Cx);
     
-    ID = num2str(roverID);
+    ID = roverID;
     temp = [convertCharsToStrings(ID), num2str(lat,10)  , num2str(long,10) , num2str(er1,6), num2str(er2,6)];
-    toSend = join(temp, ",")
+    toSend = join(temp, ",");
     java.lang.Thread.sleep(300);
     fprintf(guiclient,'%s\n',toSend);
+    
+    for i=1:size(targets,1)
+        [lat, long] = GetLatLong(lat_o, long_o, targets(i,1), targets(i,2));
+        ID = targetList(1,i);
+        er1 = 0;
+        er2 = 0;
+        temp = [convertCharsToStrings(ID), num2str(lat,10)  , num2str(long,10) , num2str(er1,6), num2str(er2,6)];
+        toSend = join(temp, ",");
+        java.lang.Thread.sleep(300);
+        fprintf(guiclient,'%s\n',toSend);
+    end
+
 end
 
