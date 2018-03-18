13cf46c8ce3852baefe58e1022bb8ca3bcf85ec1
Dylan <dylan0951@live.com>
2018-03-18 14:44:04 -0600

    Second line fit included in positioning ALGO
    -Sundaty stationary test results added

--- PositioningAlgo/REAL/RssiToDistanceLineFit.m
+++ PositioningAlgo/REAL/RssiToDistanceLineFit.m
@@ -1,5 +1,8 @@
-function [ distance ] = RssiToDistanceLineFit( a, b, rssi )
-distance = (rssi - b) ./ a;
+function [ distance ] = RssiToDistanceLineFit( a1, b1, a2, b2, rssi )
+distance = (rssi - b1) ./ a1;
+if distance < 0
+    distance = (rssi - b2) ./ a2;
+end
 if distance < 0
     distance = 0;
 end
--- PositioningAlgo/REAL/main.m
+++ PositioningAlgo/REAL/main.m
@@ -14,26 +14,28 @@ fopen(guiclient);
 roverID = "Node4";
 
 %RSSI to Distance Conversion Param
-a = -0.69;
-b = -48.7;
+a1 = -0.69;
+b1 = -48.7;
+a2 = -3.79;
+b2 = -37.55;
 ref = -36.59;
 
 %Kalman Filter Param
 Q = eye(2,2);
 initializeKalman = false;
-x_original = [6.7, 5.1]; % Change for the program
-targets = [0, 0; 0, 9.10; 14.9, 8.2; 14.9, 2.5; 6.7, 0]; %Change
-targetList = ["Node1", "Node0", "Node3", "Node5", "Node2"]; %Change
+x_original = [7.4, 4.4]; % Change for the program
+targets = [0, 0; 6.5, 0; 14.9, 0; 14.9, 8.4; 0, 8.04]; %Change
+targetList = ["Node3", "Node2", "Node1", "Node0", "Node5"]; %Change
 
 %Convert to Lat Long
 lat_o = 51.079948;
 long_o =  -114.125534;
 
-%Storage Constants
-raw_rssi = zeros(1,3);
-conv_dist = zeros(1,3);
-kalman_coords = zeros(1,3);
-kalman_delta = zeros(1,3);
+%Open File
+RawRssi = fopen('RawRssi.txt', 'w');
+ConvertedDist = fopen('ConvertedDist.txt','w');
+Coordinates = fopen('Coordinates.txt','w');
+Delta = fopen('Delta.txt','w');
 
 %% While program is running
 while(1)
@@ -55,29 +57,29 @@ while(1)
     rssi = GetRSSI(NodeList, roverID, targetList);
     
     for i=1:size(rssi,2)
-        raw_rssi = [raw_rssi; [time, targetList(1,i), rssi(1,i)]];
+        fprintf(RawRssi, '%s,%s,%s\n', [time, targetList(1,i), rssi(1,i)]');
     end
             
     % Convert RSSI to Distance
     for i = 1:size(rssi,2)
-        dist(1,i) = RssiToDistanceLineFit(a, b, rssi(1,i));
+        dist(1,i) = RssiToDistanceLineFit(a1, b1, a2, b2, rssi(1,i));
     end
     
     for i=1:size(dist,2)
-        conv_dist = [conv_dist; [time, targetList(1,i), dist(1,i)]];
+        fprintf(ConvertedDist, '%s,%s,%s\n', [time, targetList(1,i), dist(1,i)]');
     end
 
     % Kalman Filter
     if initializeKalman == false
         [KF_Coords, KF_Cx, KF_dx] = InitializeKF(x_original, targets, dist);
-        kalman_coords = [kalman_coords; [time, KF_Coords(1,1), KF_Coords(1,2)]];
-        kalman_delta = [kalman_delta; [time, KF_dx(1,1), KF_dx(2,1)]];
+        fprintf(Coordinates, '%s,%s,%s\n', [time, KF_Coords(1,1), KF_Coords(1,2)]');
+        fprintf(Delta, '%s,%s,%s\n', [time, KF_dx(1,1), KF_dx(2,1)]');
         initializeKalman = true;
         terminate = SentToGui(roverID, guiclient, KF_Coords, lat_o, long_o, KF_Cx, targets, targetList);
     else
         [KF_Coords, KF_Cx, KF_dx] = KalmanFiltering(dist, targets, KF_Coords, KF_Cx, KF_dx, Q);
-        kalman_coords = [kalman_coords; [time, KF_Coords(1,1), KF_Coords(1,2)]];
-        kalman_delta = [kalman_delta; [time, KF_dx(1,1), KF_dx(2,1)]];
+        fprintf(Coordinates, '%s,%s,%s\n', [time, KF_Coords(1,1), KF_Coords(1,2)]');
+        fprintf(Delta, '%s,%s,%s\n', [time, KF_dx(1,1), KF_dx(2,1)]');
         terminate = SentToGui(roverID, guiclient, KF_Coords, lat_o, long_o, KF_Cx, targets, targetList);
     end
     
@@ -100,18 +102,7 @@ delete(guiclient);
 clear guiclient
 
 %% Print to File
-RawRssi = fopen('RawRssi.txt', 'w');
-fprintf(RawRssi, '%s,%s,%s\n', raw_rssi');
 fclose(RawRssi);
-
-ConvertedDist = fopen('ConvertedDist.txt','w');
-fprintf(ConvertedDist, '%s,%s,%s\n', conv_dist');
 fclose(ConvertedDist);
-
-Coordinates = fopen('Coordinates.txt','w');
-fprintf(Coordinates, '%s,%s,%s\n', kalman_coords');
 fclose(Coordinates);
-
-Delta = fopen('Delta.txt','w');
-fprintf(Delta, '%s,%s,%s\n', kalman_delta');
 fclose(Delta);
